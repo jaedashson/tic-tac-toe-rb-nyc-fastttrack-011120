f017eb6ae5ed40b9eda58ac83ce08e74f939bb7a
Jae-Son Song <jaesonsong@gmail.com>
2020-01-11 21:48:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -133,6 +133,7 @@ end
 ################################################################################
 def play(board)
   puts "Welcome to Tic Tac Toe!"
+
   until over?(board)
     turn(board)
   end
